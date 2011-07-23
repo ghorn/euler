72ed7dec185a340d29cd48819aa732fe852b7586
Greg Horn <gregmainland@gmail.com>
2011-07-22 19:46:26 -0700

    add brute force problem 34 outline

--- euler.hs
+++ euler.hs
@@ -293,6 +293,22 @@ e18 = head $foldr (\x acc -> zipWith (+) x (maxNeighbor' acc)) (last tree) (init
 --    maxNeighbor acc (x0:x1:xs) = maxNeighbor (acc ++ [max x0 x1]) (x1:xs)
 
 
+-- (non-working brute force solution) sum of all numbers whose sum of factorial of digits == number
+e34 :: Integer
+e34 = sum $ filter good [1..10000]
+  where
+    splitIntegral :: (Integral a, Read a) => a -> [a]
+    splitIntegral a = map (\x -> read x) $ map (\x -> [x]) (show a)
+
+    fact :: (Integral a) => a -> a
+    fact 0 = 1
+    fact n = n *(fact (n-1))
+
+    sumFact n = sum $ map fact (splitIntegral n)
+    good :: Integer -> Bool
+    good n = (sumFact n) == n
+
+
 -- greatest sum along large binary tree
 e67 :: String -> Integer
 e67 fileText = head $foldr (\x acc -> zipWith (+) x (maxNeighbor' acc)) (last tree) (init tree)
